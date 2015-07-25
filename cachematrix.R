e320d386751f96a7573a7bca32219375a1be2081
nangemo <nangemo@users.noreply.github.com>
2015-07-25 22:36:05 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ##I am using two functions, makeCacheMatrix() and cacheSolve() to calculate the inverse matrix of a give matrix, 
 ##based on Object-Oriented Programming(OOP), which is efficient using the caching feature of R
 
-## Based on the example given in the assignment question, i did a few crucial modifications to get the
+## Based on the example given in the assignment question, I did a few crucial modifications to get the
 ## desired the result.
-## As for the first function, it is designed to creat an object which i will assess  in the second function
-## within this function i defined four internal functions, set(), get(), setsolve()and getsolve().
+## As for the first function, it is designed to become a place where storing information now (in itself) or later ( in cacheSolve() function） that we are going to refer in cacheSolve() later 
+## within this function I defined four internal functions, set(), get(), setsolve()and getsolve().
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -20,15 +20,15 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Based on the example given in the assignment question, i did a few crucial modification to get the
+## Based on the example given in the assignment question, I did a few crucial modification to get the
 ## desired the result.
 ## As for the second function, it is designed to assess the internal functions defined in the first funtion
-## whithin this function i firstly assess the getsolve()function which defined in previous function, then i 
-## i use the if() to figure out whether the targeted matrix is calculated by R before or not. If it is repeated
+## whithin this function I firstly assess the getsolve()function which defined in previous function, then I
+## use the if() to figure out whether the targeted matrix is calculated by R before or not. If it is repeated
 ## matrix, the second function will just caching the data from the "the storage device" , function 1,to returen the 
 ## result, inverse matrix. However, if the targetd matrix is a new matrix which haven't been calculated or assessed before
 ## then, the secondfunction, cacheSolve, will start to calculate the inverse matrix of this new targeted matrix
-## and mark it as an assessed matrix through the function setsolve()
+## and mark it as an assessed matrix through the function setsolve(), which is the situation I previously mentioned "later storing information situation"
 
 cacheSolve <- function(x, ...) {
   i <- x$getsolve()
